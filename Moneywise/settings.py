2dc43e6aa5e5d6e9e8ac9195154dc8f7565f8ca8
Sandesh Dhakal <sdhakal712@gmail.com>
2025-07-25 14:30:00 +0545

    Update settings.py

--- Moneywise/settings.py
+++ Moneywise/settings.py
@@ -6,7 +6,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('django-insecure-!%zfw$9bkf^!bd)z9(i)bju52$5r3-%5d5#qlh+a&5aiiirx*!', 'OV6BzydeDzCBZrJSoFLU2tK_XYgEfRGV1T6aJYe39ahg5aS87-RX3HHbxcDb898rvtA')
 
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = True
 ALLOWED_HOSTS = ['moneywise-5szt.onrender.com']
 
 INSTALLED_APPS = [
